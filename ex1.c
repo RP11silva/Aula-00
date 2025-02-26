4b023a3eae83abf53b35c34e536c58b71e39d59b
RP11silva <rafinhap11silva@gmail.com>
2025-02-26 10:03:42 -0300

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int numero;
+  printf("digite um numero");
+  scanf("%d", &numero);
+  
+
+  if (numero > 10);
+     printf("Sim\n");
+  return 0;
+    
+    }
